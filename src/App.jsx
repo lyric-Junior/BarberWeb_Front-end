3200f853ccc4ae004873a8c6c8b7fde5988e84d5
toucan_it <formerlyric16@gmail.com>
2026-06-04 15:41:47 -0300

    Tailwindcss config and testing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div className='w-100% h-20 flex justify-around p-2 bg'>
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
